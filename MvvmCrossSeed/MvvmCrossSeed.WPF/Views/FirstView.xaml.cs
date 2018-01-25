e3af0fc449b99cb67fc6808e853c8dbfa56aeaeb
motor2stags <motor2stags@gmail.com>
2018-01-26 00:45:49 +0900

    プロジェクト ファイルを追加します。

---
+++ MvvmCrossSeed/MvvmCrossSeed.WPF/Views/FirstView.xaml.cs
@@ -0,0 +1,12 @@
+using MvvmCross.Wpf.Views;
+
+namespace MvvmCrossSeed.WPF.Views
+{
+    public partial class FirstView : MvxWpfView
+    {
+        public FirstView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
